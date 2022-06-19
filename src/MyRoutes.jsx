567e1ec4667cc518474fc3b6d8117bf91705349b
Aarseus <trojanbista@gmail.com>
2022-06-19 08:26:13 +0545

    first

---
+++ src/MyRoutes.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+
+const MyRoutes = () => {
+  return (
+    <>
+
+  <Router>
+    <Routes>
+        <Route/>
+    </Routes>
+  </Router>
+    </>
+  )
+}
+
+export default MyRoutes
---
+++ src/Reducers/CounterReducer.jsx
@@ -0,0 +1,8 @@
+const initialData={
+    counter: 0
+}
+const CounterReducer=(state=initialData)=>{
+    return counter
+}
+
+export default CounterReducer
